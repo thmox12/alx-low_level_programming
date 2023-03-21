33c5545fb30377753da47e5b07af8f891760b25e
thmox12 <brahim.ouhimi@gmail.com>
2023-03-21 03:06:55 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*main - Entry point of the program
+*Description:  program that prints _putchar, followed by a new line.
+*Return: Success (0)
+*/
+int main(void)
+{
+char mychars[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+int i = 0;
+while (i < 8)
+	{
+		_putchar(mychars[i]);
+		i++;
+	}
+_putchar('\n');
+return (0);
+}
